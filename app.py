2573276e44331110876a6b08f36ac951bec6a80d
mphil17 <mattphil48@gmail.com>
2020-11-25 18:35:56 +0000

    Copied over index.html, script.js, style.css.

---
+++ app.py
@@ -0,0 +1,47 @@
+import os
+from flask import (Flask, render_template, url_for, redirect, request,
+flash, session)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
+if os.path.exists("env.py"):
+    import env
+
+app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signUp():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+             {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            redirect(url_for("signUp"))
+
+        signup = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(signup)
+
+        session["user"] = request.form.get("username").lower()
+        flash("Successfully Signed Up")
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
---
+++ static/js/script.js
@@ -0,0 +1,158 @@
+/*----------------feelings buttons--------------------*/
+function happierButtonClick() {
+    $(".message-box").text("You are happier");
+    document.getElementById("happier-button").disabled = true;
+    document.getElementById("happy-button").disabled = true;
+    document.getElementById("meh-button").disabled = true;
+    document.getElementById("sad-button").disabled = true;
+    document.getElementById("sadder-button").disabled = true;
+    document.getElementById("happier-button").style.opacity = 0.7;
+    document.getElementById("happy-button").style.opacity = 0.7;
+    document.getElementById("meh-button").style.opacity = 0.7;
+    document.getElementById("sad-button").style.opacity = 0.7;
+    document.getElementById("sadder-button").style.opacity = 0.7;
+    document.getElementById("happier-button").classList.remove("button-st");
+    document.getElementById("happy-button").classList.remove("button-st");
+    document.getElementById("meh-button").classList.remove("button-st");
+    document.getElementById("sad-button").classList.remove("button-st");
+    document.getElementById("sadder-button").classList.remove("button-st");
+    sendData();
+}
+
+function happyButtonClick() {
+    $(".message-box").text("You are happy");
+    document.getElementById("happier-button").disabled = true;
+    document.getElementById("happy-button").disabled = true;
+    document.getElementById("meh-button").disabled = true;
+    document.getElementById("sad-button").disabled = true;
+    document.getElementById("sadder-button").disabled = true;
+    document.getElementById("happier-button").style.opacity = 0.7;
+    document.getElementById("happy-button").style.opacity = 0.7;
+    document.getElementById("meh-button").style.opacity = 0.7;
+    document.getElementById("sad-button").style.opacity = 0.7;
+    document.getElementById("sadder-button").style.opacity = 0.7;
+    document.getElementById("happier-button").classList.remove("button-st");
+    document.getElementById("happy-button").classList.remove("button-st");
+    document.getElementById("meh-button").classList.remove("button-st");
+    document.getElementById("sad-button").classList.remove("button-st");
+    document.getElementById("sadder-button").classList.remove("button-st");
+}
+
+function mehButtonClick() {
+    $(".message-box").text("You are ok");
+    document.getElementById("happier-button").disabled = true;
+    document.getElementById("happy-button").disabled = true;
+    document.getElementById("meh-button").disabled = true;
+    document.getElementById("sad-button").disabled = true;
+    document.getElementById("sadder-button").disabled = true;
+    document.getElementById("happier-button").style.opacity = 0.7;
+    document.getElementById("happy-button").style.opacity = 0.7;
+    document.getElementById("meh-button").style.opacity = 0.7;
+    document.getElementById("sad-button").style.opacity = 0.7;
+    document.getElementById("sadder-button").style.opacity = 0.7;
+    document.getElementById("happier-button").classList.remove("button-st");
+    document.getElementById("happy-button").classList.remove("button-st");
+    document.getElementById("meh-button").classList.remove("button-st");
+    document.getElementById("sad-button").classList.remove("button-st");
+    document.getElementById("sadder-button").classList.remove("button-st");
+}
+
+
+function sadButtonClick() {
+    $(".message-box").text("You are sad");
+    document.getElementById("happier-button").disabled = true;
+    document.getElementById("happy-button").disabled = true;
+    document.getElementById("meh-button").disabled = true;
+    document.getElementById("sad-button").disabled = true;
+    document.getElementById("sadder-button").disabled = true;
+    document.getElementById("happier-button").style.opacity = 0.7;
+    document.getElementById("happy-button").style.opacity = 0.7;
+    document.getElementById("meh-button").style.opacity = 0.7;
+    document.getElementById("sad-button").style.opacity = 0.7;
+    document.getElementById("sadder-button").style.opacity = 0.7;
+    document.getElementById("happier-button").classList.remove("button-st");
+    document.getElementById("happy-button").classList.remove("button-st");
+    document.getElementById("meh-button").classList.remove("button-st");
+    document.getElementById("sad-button").classList.remove("button-st");
+    document.getElementById("sadder-button").classList.remove("button-st");
+}
+
+function sadderButtonClick() {
+    $(".message-box").text("You are sadder");
+    document.getElementById("happier-button").disabled = true;
+    document.getElementById("happy-button").disabled = true;
+    document.getElementById("meh-button").disabled = true;
+    document.getElementById("sad-button").disabled = true;
+    document.getElementById("sadder-button").disabled = true;
+    document.getElementById("happier-button").style.opacity = 0.7;
+    document.getElementById("happy-button").style.opacity = 0.7;
+    document.getElementById("meh-button").style.opacity = 0.7;
+    document.getElementById("sad-button").style.opacity = 0.7;
+    document.getElementById("sadder-button").style.opacity = 0.7;
+    document.getElementById("happier-button").classList.remove("button-st");
+    document.getElementById("happy-button").classList.remove("button-st");
+    document.getElementById("meh-button").classList.remove("button-st");
+    document.getElementById("sad-button").classList.remove("button-st");
+    document.getElementById("sadder-button").classList.remove("button-st");
+}
+
+// changes made to webpage when user logs in
+function loggedInObjects() {
+    let signUp = document.getElementById("signUp");
+    signUp.style.display = "none";
+    let signIn = document.getElementById("signIn");
+    signIn.style.display = "none";
+    let signOut = document.getElementById("signOut");
+    signOut.style.display = "inline-block";
+    let loggedIn = document.getElementById("loggedIn");
+    loggedIn.style.display = "inline-block";
+    let happier = document.getElementById("happier-button");
+    happier.style.display = "flex";
+    let happy = document.getElementById("happy-button");
+    happy.style.display = "flex";
+    let meh = document.getElementById("meh-button");
+    meh.style.display = "flex";
+    let sad = document.getElementById("sad-button");
+    sad.style.display = "flex";
+    let sadder = document.getElementById("sadder-button");
+    sadder.style.display = "flex";
+    let message = document.getElementById("message");
+    message.style.display = "flex";
+    let user = document.getElementById("userInfo");
+    user.style.display = "block";
+    var emailShow = document.getElementById("email");
+    var passwordShow = document.getElementById("password");
+    emailShow.style.display = "none";
+    passwordShow.style.display = "none";
+    // tells user they are logged in
+    var thisUser = firebase.auth().currentUser;
+    let html = `<p> You are logged in as: <strong> ${thisUser.email}</strong></p>`;
+    loggedIn.innerHTML = html;
+}
+
+// changes made to webpage when user logs out
+function loggedOutObjects() {
+    let signUp = document.getElementById("signUp");
+    signUp.style.display = "inline-block";
+    let signIn = document.getElementById("signIn");
+    signIn.style.display = "inline-block";
+    let signOut = document.getElementById("signOut");
+    signOut.style.display = "none";
+    let loggedIn = document.getElementById("loggedIn");
+    loggedIn.style.display = "none";
+    let happier = document.getElementById("happier-button");
+    happier.style.display = "none";
+    let happy = document.getElementById("happy-button");
+    happy.style.display = "none";
+    let meh = document.getElementById("meh-button");
+    meh.style.display = "none";
+    let sad = document.getElementById("sad-button");
+    sad.style.display = "none";
+    let sadder = document.getElementById("sadder-button");
+    sadder.style.display = "none";
+    let message = document.getElementById("message");
+    message.style.display = "none";
+    let user = document.getElementById("userInfo");
+    user.style.display = "none";
+    loggedIn.innerHTML = "";
+}
